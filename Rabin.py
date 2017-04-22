2c503689e12769fe9112099e7c523174bce6b164
ChengAndy <chengandy@ChengAndydeMacBook-Pro.local>
2017-04-22 15:23:23 +0800

    Finished encryption @TODO decryption

---
+++ Rabin.py
@@ -0,0 +1,22 @@
+# Rabin.py
+
+
+# encryption function
+# plaintext is a 224-bit number
+def encryption(plaintext, n):
+    # c = m^2 mod n
+    plaintext = padding(plaintext)
+    return plaintext ** 2 % n
+
+
+# padding 16 bits to the end of a number
+def padding(plaintext):
+    binary_str = bin(plaintext)     # convert to a bit string
+    output = binary_str + binary_str[-16:]      # pad the last 16 bits to the end
+    return int(output, 2)       # convert back to integer
+
+
+# encryption function
+def decryption(ciphertext, n):
+    plaintext = 0
+    return plaintext
--- main.py
+++ main.py
@@ -2,22 +2,28 @@
 # Rabin Public-Key Cryptosystem
 
 import prime
+import Rabin
 
 # main()
 if __name__ == '__main__':
 
+    # p and q has 128 bits
     # p = prime.generate_a_prime_number(128)
     p = 0xdaaefe652cad1614f17e87f2cd80973f
     # q = prime.generate_a_prime_number(128)
     q = 0xf99988626723eef2a54ed484dfa735c7
     n = p*q
+
+    # plaintext has 224 bits
     plaintext = 0xbe000badbebadbadbad00debdeadfacedeafbeefadd00addbed00bed
-    ciphertext = plaintext ** 2 % n
+
+    # Rabin encryption
+    ciphertext = Rabin.encryption(plaintext, n)
 
     # 256-bit prime number generation
     print('<Miller-Rabin>')
     print(format(prime.generate_a_prime_number(256), 'x'))
-    # Encryption
+    # Show encryption information
     print('\n<Rabin Encryption>')
     print('p =', format(p, 'x'))
     print('q =', format(q, 'x'))
@@ -26,3 +32,12 @@ if __name__ == '__main__':
     print('Ciphertext =', format(ciphertext, 'x'))
 
     # Decryption
+    ciphertext = 0x5452361adb4c34be04a5903ae00793bc1086e887ebed06e23ffba0b4a4348cc0
+    p = 0xd5e68b2b5855059ad1a80dd6c5dc03eb
+    q = 0xc96c6afc57ce0f53396d3b32049fe2d3
+    plaintext = Rabin.decryption(ciphertext, n)
+    print('\n<Rabin Decryption>')
+    print('Ciphertext =', format(ciphertext, 'x'))
+    print('p =', format(p, 'x'))
+    print('q =', format(q, 'x'))
+    print('Plaintext =', format(plaintext, 'x'))
